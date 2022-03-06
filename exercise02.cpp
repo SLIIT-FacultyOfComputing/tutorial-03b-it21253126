4d47fec2e9cca5b4c0cc9f8327fcee022dbe9d68
IT21253126 (Missaka) <95597476+it21253126@users.noreply.github.com>
2022-03-06 09:42:20 +0000

    Exercise 3 complete

--- exercise02.cpp
+++ exercise02.cpp
@@ -29,5 +29,8 @@ int main() {
     
     return 0;
 }
-
-// Implement the Volume() function here
+// Implement the Volume() function 
+int volume(int height, int width, int length)
+{
+  return height * width * length;
+}
